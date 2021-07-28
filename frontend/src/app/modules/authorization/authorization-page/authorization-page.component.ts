6395c8012573f83d19099156447617082cd2dc16
KateKubyshka <ekaterina.kubyshka@gmail.com>
2021-07-28 15:03:42 +0300

    Add authorization page component

---
+++ frontend/src/app/modules/authorization/authorization-page/authorization-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authorization-page',
+  templateUrl: './authorization-page.component.html',
+  styleUrls: ['./authorization-page.component.sass']
+})
+export class AuthorizationPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
