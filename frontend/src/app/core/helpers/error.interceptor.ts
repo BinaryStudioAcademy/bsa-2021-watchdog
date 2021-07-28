e6b0847cfce968b47bb4abb180e97325f3857620
Oleksandr <oleksandr.bovchaliuk@gmail.com>
2021-07-28 17:50:48 +0300

    Add error interceptor

--- frontend/src/app/core/helpers/error.interceptor.ts
+++ frontend/src/app/core/helpers/error.interceptor.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor() {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+        retry(1),
+        catchError((error: HttpErrorResponse) => {
+            let errorMessage = '';
+            if (error.error instanceof ErrorEvent) {
+                errorMessage = `Error : ${error.error.message}`;
+            } else {
+                errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+            }
+            return throwError(errorMessage);
+        })
+    )
   }
 }
