d5940999c03cae851d02e9f7bc39a0cfa2aaaade
Vadym Kolesnyk <vakol2001@gmail.com>
2021-07-29 14:00:16 +0300

    Add platforms

--- frontend/src/app/modules/landing/landing.module.ts
+++ frontend/src/app/modules/landing/landing.module.ts
@@ -7,13 +7,17 @@ import { LandingDemoComponent } from './components/landing-demo/landing-demo.com
 import { YouTubePlayerModule } from '@angular/youtube-player';
 import { LandingContentComponent } from './components/landing-content/landing-content.component';
 import { LandingContentItemComponent } from './components/landing-content-item/landing-content-item.component';
+import { LandingPlatformsComponent } from './components/landing-platforms/landing-platforms.component';
+import { LandingFooterComponent } from './components/landing-footer/landing-footer.component';
 
 @NgModule({
     declarations: [
         LandingPageComponent,
         LandingDemoComponent,
         LandingContentComponent,
-        LandingContentItemComponent
+        LandingContentItemComponent,
+        LandingPlatformsComponent,
+        LandingFooterComponent
     ],
     imports: [
         SharedModule, LandingRoutingModule,YouTubePlayerModule
