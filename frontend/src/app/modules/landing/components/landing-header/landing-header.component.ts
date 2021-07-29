fbbdbea28fd033c540d6e0c5210b29aff3e1a2c0
Vadym Kolesnyk <vakol2001@gmail.com>
2021-07-29 17:00:25 +0300

    Add logos

---
+++ frontend/src/app/modules/landing/components/landing-header/landing-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-header',
+  templateUrl: './landing-header.component.html',
+  styleUrls: ['./landing-header.component.sass']
+})
+export class LandingHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
