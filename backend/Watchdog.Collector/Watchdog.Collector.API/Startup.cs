3e0687db3fc0e7f622b42a92ca5d2229cc9b99b9
Vova <volodymyr_bilyk@outlook.com>
2021-07-29 13:47:57 +0300

    Add angular linting action

--- backend/Watchdog.Collector/Watchdog.Collector.API/Startup.cs
+++ backend/Watchdog.Collector/Watchdog.Collector.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Serilog;
+using System;
 using System.Collections.Generic;
 
 namespace Watchdog.Collector.API
@@ -64,7 +65,7 @@ namespace Watchdog.Collector.API
                                 Id = JwtBearerDefaults.AuthenticationScheme,
                             }
                         },
-                        new string[] {}
+                        Array.Empty<string>()
                     }
                 });
             });
