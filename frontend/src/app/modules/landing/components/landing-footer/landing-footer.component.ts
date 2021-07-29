a9dba93a93085715944e4579ade235ce74dded78
Vadym Kolesnyk <vakol2001@gmail.com>
2021-07-29 14:00:33 +0300

    Init footer

---
+++ frontend/src/app/modules/landing/components/landing-footer/landing-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-footer',
+  templateUrl: './landing-footer.component.html',
+  styleUrls: ['./landing-footer.component.sass']
+})
+export class LandingFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
