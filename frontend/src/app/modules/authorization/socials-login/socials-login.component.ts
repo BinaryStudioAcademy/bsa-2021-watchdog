8d402e45682de10a48d1585ec293ac7c941ee4e3
KateKubyshka <ekaterina.kubyshka@gmail.com>
2021-07-30 11:54:32 +0300

    Add socials-login component

---
+++ frontend/src/app/modules/authorization/socials-login/socials-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-socials-login',
+  templateUrl: './socials-login.component.html',
+  styleUrls: ['./socials-login.component.sass']
+})
+export class SocialsLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
