a5f12352ef4c15ade578f3f522bcf3d0e5961477
KateKubyshka <ekaterina.kubyshka@gmail.com>
2021-07-30 11:47:52 +0300

    Add prime-components

--- frontend/src/app/shared/modules/prime-components/prime-components.module.ts
+++ frontend/src/app/shared/modules/prime-components/prime-components.module.ts
@@ -3,6 +3,9 @@ import {CommonModule} from '@angular/common';
 
 import {ButtonModule} from 'primeng/button';
 import {InputTextModule} from 'primeng/inputtext';
+import {TabViewModule} from 'primeng/tabview';
+import {PasswordModule} from 'primeng/password';
+import {CheckboxModule} from 'primeng/checkbox';
 
 
 // import and export here all required modules from primeng
@@ -10,11 +13,17 @@ import {InputTextModule} from 'primeng/inputtext';
     imports: [
         CommonModule,
         ButtonModule,
+        TabViewModule,
         InputTextModule,
+        PasswordModule,
+        CheckboxModule
     ],
     exports: [
         ButtonModule,
         InputTextModule,
+        TabViewModule,
+        PasswordModule,
+        CheckboxModule,
     ],
     declarations: []
 })
