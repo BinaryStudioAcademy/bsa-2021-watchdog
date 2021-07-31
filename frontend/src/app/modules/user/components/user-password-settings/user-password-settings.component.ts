c43d5f553a0632e947a05dd5fb9da964e28c6b97
Yuriy <yurii.solodovnyk@nure.ua>
2021-07-31 18:58:26 +0300

    Add adaptive password editor design

---
+++ frontend/src/app/modules/user/components/user-password-settings/user-password-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-password-settings',
+  templateUrl: './user-password-settings.component.html',
+  styleUrls: ['./user-password-settings.component.sass']
+})
+export class UserPasswordSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
