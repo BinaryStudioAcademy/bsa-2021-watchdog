f9838b3fc203422e8e9116973c9caedbefaee478
Vadym Kolesnyk <vakol2001@gmail.com>
2021-08-01 20:50:15 +0300

    Add options class

---
+++ backend/Watchdog.Core/Watchdog.Core.BLL/Services/Options/EmailSendOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Watchdog.Core.BLL.Services.Options
+{
+    public class EmailSendOptions
+    {
+        public string ApiKey { get; set; }
+        public string SenderEmail { get; set; }
+        public string SenderName { get; set; }
+    }
+}
