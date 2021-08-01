d3b9d036cf78bbdbba8890ec8064dae3d9dbb1fa
Vadym Kolesnyk <vakol2001@gmail.com>
2021-08-01 22:41:52 +0300

    Add extensions to add email send service

--- backend/Watchdog.Core/Watchdog.Core.API/Extensions/ServiceCollectionExtensions.cs
+++ backend/Watchdog.Core/Watchdog.Core.API/Extensions/ServiceCollectionExtensions.cs
@@ -42,5 +42,18 @@ namespace Watchdog.Core.API.Extensions
                     connectionsString,
                     opt => opt.MigrationsAssembly(typeof(WatchdogCoreContext).Assembly.GetName().Name)));
         }
+
+
+        public static void AddEmailSendService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IEmailSendService, EmailSendService>(provider => new EmailSendService(new BLL.Services.Options.EmailSendOptions
+            {
+                ApiKey = configuration["SENDGRID_API_KEY"], // you need to add SENDGRID_API_KEY to yours environment variables
+                SenderEmail = "bsa2021watchdog@gmail.com",
+                SenderName = "BSA Watchdog",
+                TemplateId = "d-b91491f5b66b47a48fd38f8365e4e075"  // templates you can create on sendgrid site
+                                                                   // for this template automatically sended in the promotions.
+            }));
+        }
     }
 }
