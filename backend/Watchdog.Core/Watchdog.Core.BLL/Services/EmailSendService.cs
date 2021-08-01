4c6ca4f9dbcd12aaa65cae130479024fd440e074
Vadym Kolesnyk <vakol2001@gmail.com>
2021-08-01 22:41:16 +0300

    Update EmailSendService.cs

--- backend/Watchdog.Core/Watchdog.Core.BLL/Services/EmailSendService.cs
+++ backend/Watchdog.Core/Watchdog.Core.BLL/Services/EmailSendService.cs
@@ -1,18 +1,36 @@
-﻿using Watchdog.Core.BLL.Services.Abstract;
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+using Watchdog.Core.BLL.Models;
+using Watchdog.Core.BLL.Services.Abstract;
+using Watchdog.Core.BLL.Services.Options;
 
 namespace Watchdog.Core.BLL.Services
 {
     public class EmailSendService : IEmailSendService
     {
-        private readonly string _key;
+        private readonly SendGridClient _client;
+        private readonly EmailAddress _sender;
+        private readonly string _templateId;
 
-        public EmailSendService(string key)
+        public EmailSendService(EmailSendOptions options)
         {
-            _key = key;
+            _client = new SendGridClient(options.ApiKey);
+            _sender = new EmailAddress(options.SenderEmail, options.SenderName);
+            _templateId = options.TemplateId;
         }
-        public string Test()
+
+        public Task<Response> SendAsync(string recipientEmail, ExampleTemplateData data)
         {
-            return _key;
+            var message = new SendGridMessage();
+
+            message.SetFrom(_sender);
+            message.AddTo(new EmailAddress(recipientEmail));
+            message.SetTemplateId(_templateId);
+            message.SetTemplateData(data);
+
+            return _client.SendEmailAsync(message);
         }
+
     }
 }
