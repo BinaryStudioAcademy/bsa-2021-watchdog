57c419e345c892e919fad72028270c26ffff2f84
egor-rusnak <yehor.rusnak9982@gmail.com>
2021-08-01 22:13:33 +0300

    add organization menu

---
+++ frontend/src/app/modules/home/organization/organization-menu/organization-menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Organization } from '@core/models/organization';
+import { BaseComponent } from '@core/components/base/base.component';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+    selector: 'app-organization-menu',
+    templateUrl: './organization-menu.component.html',
+    styleUrls: ['./organization-menu.style.sass'],
+})
+export class OrganizationMenuComponent implements OnInit {
+    organization: Organization;
+    menuItems: MenuItem[];
+
+    ngOnInit(): void {
+        //getting organization, maybe from route... or inputed from parent
+
+        this.menuItems = [
+            { label: 'Organization settings', routerLink: './settings' },
+            { label: 'Teams', routerLink: './' },
+            { label: 'Members', routerLink: './' }
+        ];
+    }
+}
