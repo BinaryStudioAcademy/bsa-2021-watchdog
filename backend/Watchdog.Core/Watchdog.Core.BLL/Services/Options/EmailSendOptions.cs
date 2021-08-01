00c6f8527856ab159a715b6a25018a8360b21cdf
Vadym Kolesnyk <vakol2001@gmail.com>
2021-08-01 22:37:43 +0300

    Add template id property

--- backend/Watchdog.Core/Watchdog.Core.BLL/Services/Options/EmailSendOptions.cs
+++ backend/Watchdog.Core/Watchdog.Core.BLL/Services/Options/EmailSendOptions.cs
@@ -5,5 +5,6 @@
         public string ApiKey { get; set; }
         public string SenderEmail { get; set; }
         public string SenderName { get; set; }
+        public string TemplateId { get; set; }
     }
 }
