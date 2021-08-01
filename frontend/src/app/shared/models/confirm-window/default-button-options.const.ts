4e57052801a5b20955e0f8f1a5b0a6629136d8b0
egor-rusnak <yehor.rusnak9982@gmail.com>
2021-08-01 12:07:35 +0300

    Lint consts for button options

--- frontend/src/app/shared/models/confirm-window/default-button-options.const.ts
+++ frontend/src/app/shared/models/confirm-window/default-button-options.const.ts
@@ -1,4 +1,4 @@
-import { PrimeIcons } from "primeng/api";
+import { PrimeIcons } from 'primeng/api';
 
 export const DefaultConfirmOptions = {
     DEFAULT_VISIBLE: true,
@@ -6,4 +6,4 @@ export const DefaultConfirmOptions = {
     DEFAULT_CANCEL: 'Cancel',
     DEFAULT_CLASS: 'p-button-primary',
     DEFAULT_ICON: PrimeIcons.INFO_CIRCLE
-}
+};
