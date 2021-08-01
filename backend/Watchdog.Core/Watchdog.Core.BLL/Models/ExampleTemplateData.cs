d971592e63650f67b95393d1eeb3bbf1ae5fa28c
Vadym Kolesnyk <vakol2001@gmail.com>
2021-08-01 21:29:14 +0300

    Add ExampleTemplateData

---
+++ backend/Watchdog.Core/Watchdog.Core.BLL/Models/ExampleTemplateData.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace Watchdog.Core.BLL.Models
+{
+
+    /* Current template
+        
+        Subject: {{subject}}
+
+        <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+        <style>
+            body {
+                background: yellow;
+            }
+        </style>
+        Hello {{name}},
+        <br /><br/>
+        I'm glad you are trying out the dynamic template feature!
+        <br /><br/>
+        I hope you are having a great day in {{city}} :)
+        <br /><br/>
+        </body>
+        </html>
+     */
+    public class ExampleTemplateData
+    {
+        [JsonProperty("subject")]
+        public string Subject { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+    }
+}
