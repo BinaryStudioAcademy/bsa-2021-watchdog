25be313bd5ace0ba342c258284aa98e5d3b67e6e
KateKubyshka <ekaterina.kubyshka@gmail.com>
2021-08-02 18:53:42 +0300

    Delete useless usings

--- frontend/src/app/modules/registration/registration.module.ts
+++ frontend/src/app/modules/registration/registration.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
-import { DropdownModule } from 'primeng/dropdown';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
