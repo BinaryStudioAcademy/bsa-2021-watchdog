1e2b6d547817b26ee28496b9433f2c51ed69fca8
egor-rusnak <yehor.rusnak9982@gmail.com>
2021-08-02 22:15:42 +0300

    Reworked styles for menu and settings

--- frontend/src/app/modules/organization/organization-menu/organization-menu.component.ts
+++ frontend/src/app/modules/organization/organization-menu/organization-menu.component.ts
@@ -1,19 +1,26 @@
+import { OrganizationService } from "@core/services/organization.service";
 import { Component, OnInit } from '@angular/core';
 import { Organization } from '@core/models/organization';
 import { BaseComponent } from '@core/components/base/base.component';
 import { MenuItem } from 'primeng/api';
+import { User } from "@core/models/user";
 
 @Component({
     selector: 'app-organization-menu',
     templateUrl: './organization-menu.component.html',
     styleUrls: ['./organization-menu.style.sass'],
 })
-export class OrganizationMenuComponent implements OnInit {
+export class OrganizationMenuComponent extends BaseComponent implements OnInit {
     organization: Organization;
     menuItems: MenuItem[];
+    currentUser: User;
+
+    constructor(private organizationService: OrganizationService) { super(); }
 
     ngOnInit(): void {
-        //getting organization, maybe from route... or inputed from parent
+        this.organizationService.getOrganization(3)
+            .pipe(this.untilThis)
+            .subscribe(o => { this.organization = o; });
 
         this.menuItems = [
             { label: 'Organization settings', routerLink: './organization/settings' },
