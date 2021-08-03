38fa6d4815fd3ad6127c1815410daf317c1a28ab
Yuriy <yurii.solodovnyk@nure.ua>
2021-08-03 19:06:05 +0300

    Create DashboardController.cs with empty methods

---
+++ backend/Watchdog.Core/Watchdog.Core.API/Controllers/DashboardController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Watchdog.Core.API.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("[controller]")]
+    public class DashboardController : ControllerBase
+    {
+        private readonly ILogger<SampleController> _logger;
+
+        public DashboardController(ILogger<SampleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<string>>> Get()
+        {
+            return Ok(new string[] { "value1", "value2" }); 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            return Ok("value");
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
