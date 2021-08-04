1d1fb06792915169f464baf4e163791c08622e74
Yuriy <yurii.solodovnyk@nure.ua>
2021-08-04 13:08:02 +0300

    Add DashboardDTO and NewDashboardDTO

---
+++ backend/Watchdog.Core/Watchdog.Core.Common/DTO/Dashboard/DashboardDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Watchdog.Core.Common.DTO.Dashboard
+{
+    public class DashboardDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int User { get; set; }
+        public string Icon { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ backend/Watchdog.Core/Watchdog.Core.Common/DTO/Dashboard/NewDashboardDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Watchdog.Core.Common.DTO.Dashboard
+{
+    public class NewDashboardDTO
+    {
+        public string Name { get; set; }
+
+        public int User { get; set; }
+        public string Icon { get; set; }
+
+        public int OrganizationId { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
