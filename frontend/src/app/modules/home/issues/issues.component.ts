0efb70db6975651253b38df7042ebfaed7158167
Vadym Kolesnyk <vakol2001@gmail.com>
2021-08-04 22:29:35 +0300

    Add p-table to issues page

--- frontend/src/app/modules/home/issues/issues.component.ts
+++ frontend/src/app/modules/home/issues/issues.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Option } from '@core/models/Option';
+import { Issue } from '@shared/models/issue/issue';
 
 @Component({
     selector: 'app-issues',
@@ -12,7 +13,36 @@ export class IssuesComponent implements OnInit {
     public sortedBy: Option;
     public sortedOptions: Option[];
     
+    public issues : Issue[];
+
+    public selectedIssues: Issue[] = [];
+
+    public timeOptions: string[];
+
+    public selectedTime: string;
+
+
     ngOnInit(): void {
+        this.setAllFieldsTemp();
+    }
+
+    selectAll(event: {checked: boolean, originalEvent: Event})
+    {
+        this.disableParentEvent(event);
+        if (event.checked) {
+            this.selectedIssues = Object.assign([], this.issues);
+        } else {
+            this.selectedIssues = [];
+        }
+        
+    }
+
+    disableParentEvent(event: { originalEvent: Event}) // to disable sorting
+    {
+        event.originalEvent.stopPropagation(); 
+    }
+
+    private setAllFieldsTemp() {
         this.countNew = {
             'all': 3,
             'secondtype': 1,
@@ -31,6 +61,25 @@ export class IssuesComponent implements OnInit {
         ]
 
         this.sortedBy = this.sortedOptions[0];
+
+        this.issues = [ ];
+
+        for (let i = 1; i <= 25; i++) {
+            const issue = {
+                name: 'TypeError' + i,
+                description: "Object [object object] has no method 'updateForm'",
+                isNew: i % 3 === 0,
+                projectTag: 'BSA-2021-1',
+                createdAt: new Date(Date.now() - (i <= 14 ? ((i - 1) * 30 * 60 * 1000) : (i * 30 * 60 * 1000 * 10))), //2 min
+                events: i,
+                users: i
+            }
+            this.issues.push(issue);       
+        }
+
+        this.timeOptions = ['24h', '14d'];
+
+        this.selectedTime = this.timeOptions[0];
 
     }
 }
