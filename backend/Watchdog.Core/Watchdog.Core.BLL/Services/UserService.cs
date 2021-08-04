025aed171b29d40e89ec28e96cb01d2a5a8a667f
Oleksandr <oleksandr.bovchaliuk@gmail.com>
2021-08-04 21:11:29 +0300

    Update UpdateUserAsync

--- backend/Watchdog.Core/Watchdog.Core.BLL/Services/UserService.cs
+++ backend/Watchdog.Core/Watchdog.Core.BLL/Services/UserService.cs
@@ -1,18 +1,29 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Watchdog.Core.BLL.Services.Abstract;
 using Watchdog.Core.Common.DTO.User;
 using Watchdog.Core.DAL.Context;
+using Watchdog.Core.DAL.Entities;
 
 namespace Watchdog.Core.BLL.Services
 {
     public class UserService : BaseService, IUserService
     {
         public UserService(WatchdogCoreContext context, IMapper mapper) : base (context, mapper) { }
-        public Task<UserDto> UpdateUserAsync(int userId, UpdateUserDto updateUser)
+        public async Task<UserDto> UpdateUserAsync(int userId, UpdateUserDto updateUser)
         {
+            var user = await _context.Users.FirstAsync(u => u.Id == userId);
+
+            var userUpdateInfo = _mapper.Map(updateUser, user);
+
+            var updatedUserInfo = _context.Update(userUpdateInfo);
+            await _context.SaveChangesAsync();
+
+            //return await GetUserAsync(updatedUserInfo.Entity.Id);
             throw new NotImplementedException();
         }
 
