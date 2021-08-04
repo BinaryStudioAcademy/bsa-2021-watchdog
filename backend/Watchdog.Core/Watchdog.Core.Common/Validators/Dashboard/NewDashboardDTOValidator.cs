634982915ad25c5c2304808689f91825f837edc9
Yuriy <yurii.solodovnyk@nure.ua>
2021-08-04 14:25:36 +0300

    Add space and dot to dashboardName expression

--- backend/Watchdog.Core/Watchdog.Core.Common/Validators/Dashboard/NewDashboardDTOValidator.cs
+++ backend/Watchdog.Core/Watchdog.Core.Common/Validators/Dashboard/NewDashboardDTOValidator.cs
@@ -3,7 +3,7 @@ using Watchdog.Core.Common.DTO.Dashboard;
 
 namespace Watchdog.Core.Common.Validators.Dashboard
 {
-    public class NewDashboardDTOValidator : AbstractValidator<NewDashboardDTO>
+    public class NewDashboardDTOValidator : AbstractValidator<NewDashboardDto>
     {
         public NewDashboardDTOValidator()
         {
@@ -11,7 +11,7 @@ namespace Watchdog.Core.Common.Validators.Dashboard
                 .NotEmpty()
                 .MinimumLength(3)
                 .MaximumLength(50)
-                .Matches(@"^[a-zA-Z0-9_]*$");
+                .Matches(@"^[a-zA-Z0-9_. ]*$");
             RuleFor(d => d.CreatedBy).NotEmpty();
         }
     }
