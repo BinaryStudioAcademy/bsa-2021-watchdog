b25c6e8e6423e5b23446cec1b91226011e604db1
Yuriy <yurii.solodovnyk@nure.ua>
2021-08-04 13:40:49 +0300

    Change validator to FluentValidation

---
+++ backend/Watchdog.Core/Watchdog.Core.Common/Validators/Dashboard/NewDashboardDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Watchdog.Core.Common.DTO.Dashboard;
+
+namespace Watchdog.Core.Common.Validators.Dashboard
+{
+    public class NewDashboardDTOValidator : AbstractValidator<NewDashboardDTO>
+    {
+        public NewDashboardDTOValidator()
+        {
+            RuleFor(d => d.Name)
+                .NotNull()
+                .MinimumLength(3)
+                .MaximumLength(50)
+                .Matches(@"^[a-zA-Z0-9_]*$");
+            RuleFor(d => d.CreatedBy).NotEmpty();
+        }
+    }
+}
--- backend/Watchdog.Core/Watchdog.Core.DAL/Entities/Dashboard.cs
+++ backend/Watchdog.Core/Watchdog.Core.DAL/Entities/Dashboard.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using Watchdog.Core.DAL.Entities.Common;
-using System.ComponentModel.DataAnnotations;
 
 namespace Watchdog.Core.DAL.Entities
 {
@@ -11,11 +10,6 @@ namespace Watchdog.Core.DAL.Entities
             Tiles = new List<Tile>();
         }
 
-        [Required(ErrorMessage = "{0} is required")]
-        [StringLength(50, MinimumLength = 3,
-        ErrorMessage = "Name should be minimum 3 characters and a maximum of 50 characters")]
-        [RegularExpression(@"^[a-zA-Z0-9_]*$")]
-        [DataType(DataType.Text)]
         public string Name { get; set; }
 
         public User User { get; set; }
