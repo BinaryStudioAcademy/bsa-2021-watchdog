bfcde4c0abba5e27f5ec1eaaacf445c7d6cf198a
Yuriy <yurii.solodovnyk@nure.ua>
2021-08-04 13:09:15 +0300

    Add validation for dashboard name and add icon field

--- backend/Watchdog.Core/Watchdog.Core.DAL/Entities/Dashboard.cs
+++ backend/Watchdog.Core/Watchdog.Core.DAL/Entities/Dashboard.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Watchdog.Core.DAL.Entities.Common;
+using System.ComponentModel.DataAnnotations;
 
 namespace Watchdog.Core.DAL.Entities
 {
@@ -10,9 +11,16 @@ namespace Watchdog.Core.DAL.Entities
             Tiles = new List<Tile>();
         }
 
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(50, MinimumLength = 3,
+        ErrorMessage = "Name should be minimum 3 characters and a maximum of 50 characters")]
+        [RegularExpression(@"^[a-zA-Z0-9_]*$")]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
 
         public User User { get; set; }
+
+        public string Icon { get; set; }
 
         public int OrganizationId { get; set; }
 
