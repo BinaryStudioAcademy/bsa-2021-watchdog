df774b94c9417a0bd3abe584a61a2e2b40f1c819
Oleksandr <oleksandr.bovchaliuk@gmail.com>
2021-08-04 20:27:29 +0300

    Base UserService

---
+++ backend/Watchdog.Core/Watchdog.Core.BLL/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Watchdog.Core.BLL.Services.Abstract;
+using Watchdog.Core.Common.DTO.User;
+using Watchdog.Core.DAL.Context;
+
+namespace Watchdog.Core.BLL.Services
+{
+    public class UserService : BaseService, IUserService
+    {
+        public UserService(WatchdogCoreContext context, IMapper mapper) : base (context, mapper) { }
+        public Task<UserDto> UpdateUserAsync(int userId, UpdateUserDto updateUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> UpdateUserPasswordAsync(int userId, UpdateUserPasswordDto updateUserPassword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
