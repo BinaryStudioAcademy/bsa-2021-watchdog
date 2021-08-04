e5735875c6507da15538d50e0683f9f4dc57b66b
Bogdan Liusik <bodialiusik@gmail.com>
2021-08-04 15:25:18 +0300

    fix routing

--- frontend/src/app/modules/team/team-routing.module.ts
+++ frontend/src/app/modules/team/team-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { TeamsComponent } from '@modules/team/teams/teams.component';
 
 const routes: Routes = [{
-    path: 'teams',
+    path: '',
     component: TeamsComponent
 }, {
     path: '',
