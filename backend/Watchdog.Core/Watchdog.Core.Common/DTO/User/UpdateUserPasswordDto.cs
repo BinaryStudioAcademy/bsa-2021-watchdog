62f36b89f728098dcb0242993bc479542842f617
Oleksandr <oleksandr.bovchaliuk@gmail.com>
2021-08-04 20:18:16 +0300

    Add user dto

---
+++ backend/Watchdog.Core/Watchdog.Core.Common/DTO/User/UpdateUserPasswordDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Watchdog.Core.Common.DTO.User
+{
+    public class UpdateUserPasswordDto
+    {
+        public string PasswordHash { get; set; }
+    }
+}
---
+++ backend/Watchdog.Core/Watchdog.Core.Common/DTO/User/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Watchdog.Core.Common.DTO.User
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
