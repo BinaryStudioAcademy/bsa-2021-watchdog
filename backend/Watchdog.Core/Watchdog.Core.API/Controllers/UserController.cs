41c02d9d55120015f2ddeba998321b76526380ba
Oleksandr <oleksandr.bovchaliuk@gmail.com>
2021-08-04 20:37:05 +0300

    UserController

---
+++ backend/Watchdog.Core/Watchdog.Core.API/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Watchdog.Core.BLL.Services.Abstract;
+using Watchdog.Core.Common.DTO.User;
+
+namespace Watchdog.Core.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserService _userService;
+
+        public UserController(ILogger<UserController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+        [HttpPut("{userId:int}")]
+        public async Task<ActionResult<UserDto>> UpdateUserAsync(int userId, UpdateUserDto updateUserDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var updateUser = await _userService.UpdateUserAsync(userId, updateUserDto);
+            return Ok(updateUser);
+        }
+    }
+}
