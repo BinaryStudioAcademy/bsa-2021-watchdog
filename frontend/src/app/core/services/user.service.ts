46092db076a54820acbb04ee1275c07f525bfd98
Oleksandr <oleksandr.bovchaliuk@gmail.com>
2021-08-05 18:46:08 +0300

    Add service for users

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -9,7 +9,22 @@ import { HttpInternalService } from './http-internal.service';
 export class UserService {
     constructor(private http: HttpInternalService) { }
 
+    public getUserById(id: number) {
+        return this.http.getFullRequest<User>(`${environment.coreUrl}/user`, { id });
+    }
+
     public updateUser(user: User) {
         return this.http.putFullRequest<User>(`${environment.coreUrl}/user`, user);
+    }
+
+    public copyUser({ id, firstName, lastName, email, avatar, password }: User) {
+        return {
+            id,
+            firstName,
+            lastName,
+            email,
+            avatar,
+            password
+        };
     }
 }
