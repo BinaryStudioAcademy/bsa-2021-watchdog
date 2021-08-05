9dd9cdb8833dbccd69271f4684f1d146fd1ada46
KateKubyshka <ekaterina.kubyshka@gmail.com>
2021-08-05 14:52:01 +0300

    Add validators to member dto

---
+++ backend/Watchdog.Core/Watchdog.Core.Common/Validators/Members/NewMemberDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Watchdog.Core.Common.DTO.Members;
+
+namespace Watchdog.Core.Common.Validators.Members
+{
+    public class NewMemberDtoValidator : AbstractValidator<NewMemberDto>
+    {
+        public NewMemberDtoValidator()
+        {
+            RuleFor(m => m.OrganizationId)
+                        .NotNull();
+            RuleFor(m => m.RoleId)
+                        .NotNull();
+            RuleFor(m => m.Email)
+                        .EmailAddress()
+                        .MinimumLength(5)
+                        .MaximumLength(30);
+            RuleFor(m => m.TeamId)
+                        .NotNull();
+        }
+    }
+}
---
+++ backend/Watchdog.Core/Watchdog.Core.Common/Validators/Members/UpdateMemberDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Watchdog.Core.Common.DTO.Members;
+
+namespace Watchdog.Core.Common.Validators.Members
+{
+    public class UpdateMemberDtoValidator : AbstractValidator<UpdateMemberDto>
+    {
+        public UpdateMemberDtoValidator() 
+        {
+            RuleFor(m => m.Id)
+                        .NotNull();
+            RuleFor(m => m.RoleId)
+                        .NotNull();
+            RuleFor(m => m.TeamId)
+                        .NotNull();
+        }
+    }
+}
