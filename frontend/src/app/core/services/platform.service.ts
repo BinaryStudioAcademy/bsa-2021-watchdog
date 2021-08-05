ca3d78183543b8a82c8ad0ac81e2483008e66b7e
Vadym Kolesnyk <vakol2001@gmail.com>
2021-08-06 01:15:26 +0300

    Add platform service

---
+++ frontend/src/app/core/services/platform.service.ts
@@ -0,0 +1,17 @@
+import { HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Platform } from '@shared/models/platforms/platform';
+import { Observable } from 'rxjs';
+import { HttpInternalService } from './http-internal.service';
+
+@Injectable({ providedIn: 'root' })
+export class PlatformService {
+
+    public readonly routePrefix = '/platform';
+
+    constructor(private httpService: HttpInternalService) { }
+
+    public getPlatforms(): Observable<HttpResponse<Platform[]>> {
+        return this.httpService.getFullRequest(`${this.routePrefix}`);
+    }
+}
