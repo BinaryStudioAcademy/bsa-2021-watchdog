d55c21dd057a8c5aef555b4070acf063991c6b78
Yuriy <yurii.solodovnyk@nure.ua>
2021-08-05 16:49:07 +0300

    Create TileDto.cs

---
+++ backend/Watchdog.Core/Watchdog.Core.Common/DTO/Tile/TileDto.cs
@@ -0,0 +1,16 @@
+﻿using Watchdog.Core.Common.DTO.Dashboard;
+using Watchdog.Core.Common.DTO.User;
+
+namespace Watchdog.Core.Common.DTO.Tile
+{
+    public class TileDto
+    {
+        public string Name { get; set; }
+
+        public UserDto User { get; set; }
+
+        public int DashboardId { get; set; }
+
+        public DashboardDto Dashboard { get; set; }
+    }
+}
