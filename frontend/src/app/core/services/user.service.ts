e97189974ffc71f2f9fee7a1e098eb283163b07a
Oleksandr <oleksandr.bovchaliuk@gmail.com>
2021-08-06 19:40:40 +0300

    Test user service

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
 import { HttpInternalService } from './http-internal.service';
@@ -11,12 +11,30 @@ import { HttpInternalService } from './http-internal.service';
 export class UserService {
     constructor(private http: HttpInternalService) { }
 
+    private currentUserSubject: BehaviorSubject<User> = new BehaviorSubject<User>(null);
+
     public getUserById(id: number): Observable<HttpResponse<User>> {
         return this.http.getFullRequest<User>(`${environment.coreUrl}/user`, { id });
     }
 
     public updateUser(user: User): Observable<HttpResponse<User>> {
         return this.http.putFullRequest<User>(`${environment.coreUrl}/user`, user);
+    }
+
+    public getCurrentUser() {
+        return this.getCurrentUserSubject;
+    }
+
+    public getAndUpdateUser() {
+        this.getUserById(2).subscribe(
+            (user) => {
+                this.updateUser(user);
+            }
+        )
+    }
+
+    public get getCurrentUserSubject(): User {
+        return this.currentUserSubject.value;
     }
 
 }
