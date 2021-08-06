0fb99e08a8be5cfe238ec40bd36a5851078a8760
KateKubyshka <ekaterina.kubyshka@gmail.com>
2021-08-06 20:30:20 +0300

    Add RoleDto

---
+++ backend/Watchdog.Core/Watchdog.Core.BLL/MappingProfiles/RoleProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Watchdog.Core.Common.DTO.Role;
+using Watchdog.Core.DAL.Entities;
+
+namespace Watchdog.Core.BLL.MappingProfiles
+{
+    public class RoleProfile : Profile
+    {
+        public RoleProfile()
+        {
+            CreateMap<Role, RoleDto>();
+            CreateMap<RoleDto, Role>();
+        }
+    }
+}
