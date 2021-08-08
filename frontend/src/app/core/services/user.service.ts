9fb7eed36eb75b7c551582667fbb7823d6964ab8
Vladyslav Maryniak <vladyslav.maryniak@gmail.com>
2021-08-08 20:02:44 +0300

    Add HTTP requests to UserService

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
+import { User } from '@core/models/user';
 import { environment } from 'src/environments/environment';
-import { User } from '../models/user';
 import { HttpInternalService } from './http-internal.service';
 
 @Injectable({
@@ -8,6 +8,14 @@ import { HttpInternalService } from './http-internal.service';
 })
 export class UserService {
     constructor(private http: HttpInternalService) { }
+
+    public getUser(uid: string) {
+        return this.http.getFullRequest<User>(`${environment.coreUrl}/user/${uid}`);
+    }
+
+    public createUser(user: User) {
+        return this.http.postFullRequest<User>(`${environment.coreUrl}/user`, user);
+    }
 
     public updateUser(user: User) {
         return this.http.putFullRequest<User>(`${environment.coreUrl}/user`, user);
