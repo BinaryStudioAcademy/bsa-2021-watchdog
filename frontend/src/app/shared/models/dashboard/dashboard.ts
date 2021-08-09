af1011220cac6bad68b9f69c216f6652573637bd
Yuriy <yurii.solodovnyk@nure.ua>
2021-08-09 14:13:00 +0300

    Remove tile list from dashboard dto

--- frontend/src/app/shared/models/dashboard/dashboard.ts
+++ frontend/src/app/shared/models/dashboard/dashboard.ts
@@ -1,10 +1,7 @@
-import { Tile } from '../tile/tile';
-
 export interface Dashboard {
     id: number,
     name: string,
     icon: string,
     createdBy: number,
     organizationId: number
-    tiles: Tile[]
 }
