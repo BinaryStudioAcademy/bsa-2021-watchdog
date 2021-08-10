35dbe93b5244fa89a38c9178db8383fe1300833f
Vadym Kolesnyk <vakol2001@gmail.com>
2021-08-11 00:21:01 +0300

    Add backend models for alerts

---
+++ backend/Watchdog.Core/Watchdog.Core.DAL/Entities/AlertSettings/AlertSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Watchdog.Core.DAL.Entities.AlertSettings
+{
+    public class AlertSettings
+    {
+        public AlertCategory AlertCategory { get; set; }
+        /// <summary>
+        /// Will be not null only if AlertCategory is Special
+        /// </summary>
+        public SpecialAlertSetting SpecialAlertSetting { get; set; }
+    }
+
+}
