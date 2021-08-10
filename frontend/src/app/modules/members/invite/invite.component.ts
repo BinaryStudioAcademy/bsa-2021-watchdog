8031ec4f0ee76ec3324e746edee08107b355b0d1
KateKubyshka <ekaterina.kubyshka@gmail.com>
2021-08-11 00:20:43 +0300

    Add invite members component

--- frontend/src/app/modules/members/invite/invite.component.ts
+++ frontend/src/app/modules/members/invite/invite.component.ts
@@ -1,10 +1,111 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { BaseComponent } from '@core/components/base/base.component';
+import { MemberService } from '@core/services/member.service';
+import { Member } from '@shared/models/member/member';
+import { OverlayPanel } from 'primeng/overlaypanel';
+import { Subject } from 'rxjs';
+import { debounceTime, first, switchMap } from 'rxjs/operators';
+import { RoleService } from '@core/services/role.service';
+import { Role } from '@shared/models/role/role';
+import { Team } from '@shared/models/projects/team';
+import { NewMember } from '@shared/models/member/new-member';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-invite',
     templateUrl: './invite.component.html',
     styleUrls: ['./invite.component.sass']
 })
-export class InviteComponent {
+export class InviteComponent extends BaseComponent implements OnInit {
     @Output() closeModal = new EventEmitter<void>();
+    members: Member[];
+    memberGroup: FormGroup;
+    searchTerm: Subject<string> = new Subject<string>();
+    roles: Role[];
+    role: Role = {} as Role;
+    teams: Team[];
+    newMember = {} as NewMember;
+
+    invations: { member: NewMember, groupForm: FormGroup }[] = [{ member: {} as NewMember, groupForm: this.generateGroupForm() }];
+
+    constructor(private memberService: MemberService, private roleService: RoleService) {
+        super();
+    }
+
+    ngOnInit(): void {
+        this.loadData();
+        this.teams = [
+            {
+                id: 1,
+                name: "team1"
+            },
+            {
+                id: 2,
+                name: "team2"
+            }
+        ]
+    }
+
+    generateGroupForm() {
+        return new FormGroup({
+            name: new FormControl(
+                '',
+                [
+                    Validators.required,
+                ]
+            ),
+            team: new FormControl(
+                '',
+                [
+                    Validators.required,
+                ]
+            ),
+            role: new FormControl(
+                '',
+                [
+                    Validators.required,
+                ]
+            )
+        });
+    }
+
+    loadData() {
+        this.searchTerm.pipe(
+            this.untilThis,
+            debounceTime(300),
+            switchMap((term: string) =>
+                this.memberService.searchMembersNotInOrganization(2, term)
+                    .pipe(this.untilThis)
+            )).subscribe(members => {
+                this.members = members;
+            });
+
+        this.roleService.getRoles()
+            .pipe(this.untilThis)
+            .subscribe(roles => this.roles = roles)
+    }
+
+    search(event: any, op: OverlayPanel, value: string) {
+        if (value !== '') {
+            this.members = [];
+            this.searchTerm.next(value);
+            op.show(event);
+        }
+        else {
+            op.hide();
+        }
+    }
+
+    invate(member: NewMember) {
+        alert(JSON.stringify(member));
+    }
+
+    addNew() {
+        this.invations = this.invations.concat({ member: {} as NewMember, groupForm: this.generateGroupForm() });
+    }
+    func() {
+        alert('blured!');
+    }
+
 }
+
