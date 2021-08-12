fe584cfac9020545cb21f2ab30f64c9ef6f8365e
Vitaliy Shatskiy <kaptowkabest@gmail.com>
2021-08-13 01:56:01 +0300

    fix: params, build-in styles for overflow. feat: error.interceptor message handling.

--- frontend/src/app/core/services/toast-notification.service.ts
+++ frontend/src/app/core/services/toast-notification.service.ts
@@ -5,46 +5,63 @@ import { Message, MessageService } from 'primeng/api';
     providedIn: 'root'
 })
 export class ToastNotificationService {
+    private errorCodeWithMessageStrings = { codeString: 'Error Code: ', messageString: '\nMessage: ' };
+    private errorString = 'Error: ';
+
     constructor(private messageService: MessageService) {
     }
 
-    info(message: string, title?: string, durationMs?: number): void {
+    info(message: string, title: string = 'Info', durationMs: number = 2000): void {
         this.message({
             severity: 'info',
-            summary: title ?? 'Info',
+            summary: title === '' ? 'Info' : title,
             detail: message,
             closable: true,
             life: durationMs
         });
     }
 
-    success(message: string, title?: string, durationMs?: number): void {
+    success(message: string, title: string = 'Success', durationMs: number = 2000): void {
+        console.log(message);
         this.message({
             severity: 'success',
-            summary: title ?? 'Success',
+            summary: title === '' ? 'Success' : title,
             detail: message,
             closable: true,
             life: durationMs
         });
     }
 
-    warning(message: string, title?: string, durationMs?: number): void {
+    warning(message: string, title: string = 'Warn', durationMs: number = 2000): void {
         this.message({
             severity: 'warn',
-            summary: title ?? 'Warn',
+            summary: title === '' ? 'Warn' : title,
             detail: message,
             closable: true,
-            life: durationMs
+            life: durationMs,
+            sticky: true
         });
     }
 
-    error(message: string, title?: string, durationMs?: number): void {
+    error(message: string, title: string = 'Error', durationMs: number = 2000): void {
+        let errorMessage = message;
+        let errorTitle = title;
+
+        //get error message from error.interceptor
+        if (this.isInterceptorErrorCodeWithMessage(message)) {
+            const indexOfMessage = errorMessage.indexOf(this.errorCodeWithMessageStrings.messageString);
+            errorTitle = message.substr(0, indexOfMessage);
+            errorMessage = message.substr(indexOfMessage + this.errorCodeWithMessageStrings.messageString.length);
+        } else if (this.isInterceptorError(message)) {
+            errorMessage = message.substr(this.errorString.length);
+        }
+
         this.message({
             severity: 'error',
-            summary: title ?? 'Error',
-            detail: message,
+            summary: errorTitle === '' ? 'Error' : title,
+            detail: errorMessage,
             closable: true,
-            life: durationMs
+            life: durationMs,
         });
     }
 
@@ -52,11 +69,27 @@ export class ToastNotificationService {
         this.messageService.add(messageConfig);
     }
 
-    clear() {
+    clear(): void {
         this.messageService.clear();
     }
 
-    private message(message: Message) {
+    private message(message: Message): void {
         this.messageService.add(message);
+    }
+
+    private isInterceptorErrorCodeWithMessage(str: string): boolean {
+        let count = 0;
+        const searchStrings = [this.errorCodeWithMessageStrings.codeString, this.errorCodeWithMessageStrings.messageString];
+        searchStrings.forEach(val => {
+            if (str.includes(val)) {
+                count += 1;
+            }
+        });
+
+        return count === 2;
+    }
+
+    private isInterceptorError(str: string): boolean {
+        return str.includes(this.errorString);
     }
 }
