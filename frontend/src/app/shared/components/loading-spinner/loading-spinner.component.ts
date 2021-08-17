6cbe62f6d072fc71291c3947d92fb254cb06ea88
Yuriy <yurii.solodovnyk@nure.ua>
2021-08-17 18:57:34 +0300

    Delete inline styles

--- frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -6,9 +6,4 @@ import { Component, Input } from '@angular/core';
 })
 export class LoadingSpinnerComponent {
     @Input() overlay: boolean;
-    @Input() size = '50px';
-    @Input() top = '25vh';
-    @Input() left = 'calc(49% - 25px)';
-    @Input() position = 'absolute';
-    @Input() margin = '100px auto';
 }
