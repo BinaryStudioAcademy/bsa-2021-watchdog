cc69dd5b8819aeabb7004f55993bda5bcc8bf28a
Oleksandr <oleksandr.bovchaliuk@gmail.com>
2021-08-17 15:25:46 +0300

    Fix conflicts

--- frontend/src/app/modules/home/issues/issues.component.ts
+++ frontend/src/app/modules/home/issues/issues.component.ts
@@ -3,6 +3,8 @@ import { IssueService } from '@core/services/issue.service';
 import { BaseComponent } from '@core/components/base/base.component';
 import { ToastNotificationService } from '@core/services/toast-notification.service';
 import { IssueInfo } from '@shared/models/issue/issue-info';
+import { catchError, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
     selector: 'app-issues',
@@ -20,13 +22,24 @@ export class IssuesComponent extends BaseComponent implements OnInit {
 
     selectedTime: string;
 
+    paginators = false;
+
+    numberOfIssues = 0;
+
+    itemsPerPage = 10;
+
     constructor(private issueService: IssueService, private toastNotification: ToastNotificationService) {
         super();
     }
 
     ngOnInit(): void {
-        this.loadIssues();
+        //this.loadIssues();
         this.setAllFieldsTemp();
+        this.loads()
+            .pipe(this.untilThis)
+            .subscribe(() => {
+                this.showPaginator();
+            });
     }
 
     selectAll(event: { checked: boolean, originalEvent: Event }) {
@@ -42,14 +55,30 @@ export class IssuesComponent extends BaseComponent implements OnInit {
         event.originalEvent.stopPropagation();
     }
 
-    private loadIssues() {
-        this.issueService.getIssuesInfo()
-            .pipe(this.untilThis)
-            .subscribe(issues => {
-                this.issues = issues;
-            }, errorResponse => {
-                this.toastNotification.error(errorResponse, 'Error', 1500);
-            });
+    private showPaginator() {
+        this.paginators = this.numberOfIssues > this.itemsPerPage;
+    }
+
+    // private loadIssues() {
+    //     this.issueService.getIssuesInfo()
+    //         .pipe(this.untilThis)
+    //         .subscribe(issues => {
+    //             this.issues = issues;
+    //             this.numberOfIssues = issues.length;
+    //         }, errorResponse => {
+    //             this.toastNotification.error(errorResponse, 'Error', 1500);
+    //         });
+    // }
+
+    private loads() {
+        return this.issueService.getIssuesInfo()
+            .pipe(
+                tap(issues => {
+                    this.issues = issues;
+                    this.numberOfIssues = issues.length;
+                }),
+                catchError(error => of(this.toastNotification.error(error, 'Error', 1500)))
+            )
     }
 
     private setAllFieldsTemp() {
