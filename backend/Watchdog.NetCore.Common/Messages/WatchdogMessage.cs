0efde2ebd062570d4ff01de061d689868a3b66a7
Vladyslav Maryniak <vladyslav.maryniak@gmail.com>
2021-08-19 20:40:24 +0300

    Add WatchdogMessages

---
+++ backend/Watchdog.NetCore.Common/Messages/WatchdogMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Watchdog.NetCore.Common.Messages
+{
+    public class WatchdogMessage
+    {
+        public WatchdogMessage()
+        {
+            OccurredOn = DateTime.UtcNow;
+            Details = new WatchdogMessageDetails();
+        }
+
+        public DateTime OccurredOn { get; set; }
+
+        public WatchdogMessageDetails Details { get; set; }
+    }
+}
