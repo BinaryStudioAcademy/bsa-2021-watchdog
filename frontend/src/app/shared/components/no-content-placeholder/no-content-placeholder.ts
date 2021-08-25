5b9e74294ebfeb357e55915aa239908645dd33e5
Yuriy <yuriy.solodovnikk@gmail.com>
2021-08-25 10:24:54 +0300

    Remade placeholder

--- frontend/src/app/shared/components/no-content-placeholder/no-content-placeholder.ts
+++ frontend/src/app/shared/components/no-content-placeholder/no-content-placeholder.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
     selector: 'app-no-content-placeholder',
     templateUrl: './no-content-placeholder.html',
     styleUrls: ['./no-content-placeholder.sass']
 })
+
 export class NoContentPlaceholderComponent {
+    @Input() title: string;
 }
