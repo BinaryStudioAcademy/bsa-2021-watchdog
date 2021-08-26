5bef859c826d1c532a0f4ffc5366eb1725b5eb80
Yuriy <yuriy.solodovnikk@gmail.com>
2021-08-26 16:44:22 +0300

    Add more icons to dashboard in 2 rows

--- frontend/src/app/modules/home/modals/dashboard/icons-list.ts
+++ frontend/src/app/modules/home/modals/dashboard/icons-list.ts
@@ -6,5 +6,7 @@ export const icons = [
     { label: 'pi pi-map', value: 'pi-map' },
     { label: 'pi pi-image', value: 'pi-image' },
     { label: 'pi pi-images', value: 'pi-images' },
-    { label: 'pi pi-circle-off', value: 'pi-circle-off' }
+    { label: 'pi pi-circle-off', value: 'pi-circle-off' },
+    { label: 'pi pi-ticket', value: 'pi-ticket' },
+    { label: 'pi pi-tablet', value: 'pi-tablet' }
 ];
