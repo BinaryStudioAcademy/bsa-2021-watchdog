e885047909ce18f064d1a17a5667236b538d6b5a
Vladyslav Maryniak <vladyslav.maryniak@gmail.com>
2021-08-28 22:03:26 +0300

    Make stacktrace more exhaustive

--- backend/Watchdog.Collector/Watchdog.Collector.Common/Models/StackFrame.cs
+++ backend/Watchdog.Collector/Watchdog.Collector.Common/Models/StackFrame.cs
@@ -4,6 +4,7 @@ namespace Watchdog.Collector.Common.Models
     {
         public string File { get; set; }
         public string MethodName { get; set; }
+        public string ClassName { get; set; }
         public string[] Arguments { get; set; }
         public int LineNumber { get; set; }
         public int Column { get; set; }
