c7c8606c3ee048ec6c67970c93f9a1f9046275f1
egor-rusnak <yehor.rusnak9982@gmail.com>
2021-09-03 00:41:23 +0300

    Added affected users

---
+++ backend/Watchdog.Core/Watchdog.Core.DAL/Migrations/20210902211026_AffectedUsers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Watchdog.Core.DAL.Migrations
+{
+    public partial class AffectedUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AffectedUserIdentifier",
+                table: "EventMessages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AffectedUserIdentifier",
+                table: "EventMessages");
+        }
+    }
+}
