b99c862bd5440722945a20420992661b365335bb
Vadym Kolesnyk <vakol2001@gmail.com>
2021-09-04 09:35:52 +0300

    Add coments

--- backend/Watchdog.Core/Watchdog.Core.BLL/Extensions/TableFilterExtensions.cs
+++ backend/Watchdog.Core/Watchdog.Core.BLL/Extensions/TableFilterExtensions.cs
@@ -24,19 +24,29 @@ namespace Watchdog.Core.BLL.Extensions
                         p.PropertyType == typeof(string)
                         && propertyNames.Any(n => n.FirstCharToUpper() == p.Name));
 
-                var parameter = Expression.Parameter(typeof(TEntity));
+                var parameter = Expression.Parameter(typeof(TEntity), "e"); // variable e
                 var expression = properties.Select(p =>
                         {
                             var property = Expression.Property(parameter, p.Name);
+                            // e.Property
+                            
                             var callLower = Expression.Call(property, property.Type.GetMethod("ToLower", Array.Empty<Type>()));
+                            // e.Property.ToLower()         
+                           
                             var callContains = Expression.Call(
                                     callLower,
                                     property.Type.GetMethod("Contains", new[] { typeof(string) }),
                                     new[] { Expression.Constant(filterLower) });
+                            // e.Property.ToLower().Contains(filterLower)
+                            
                             return callContains;
                         })
                     .Aggregate<Expression>((left, right) => Expression.OrElse(left, right));
+                // e.Property1.ToLower().Contains(filterLower) || e.Property2.ToLower().Contains(filterLower) || ...
+                
                 var lambda = Expression.Lambda<Func<TEntity, bool>>(expression, parameter);
+                // e => e.Property1.ToLower().Contains(filterLower) || e.Property2.ToLower().Contains(filterLower) || ...
+
                 dataSet = dataSet.Where(lambda);
             }
             return dataSet.PrimengTableFilter(filterPayload, out totalRecord);
