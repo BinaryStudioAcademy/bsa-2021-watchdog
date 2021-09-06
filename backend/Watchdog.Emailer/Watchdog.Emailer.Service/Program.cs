4209be400c9f4f23eb3cafcd87332982084fab7d
Vladyslav Maryniak <vladyslav.maryniak@gmail.com>
2021-09-06 13:38:53 +0300

    Create Emailer as hosted service

---
+++ backend/Watchdog.Emailer/Watchdog.Emailer.Service/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SendGrid;
+using Watchdog.Emailer.Service.Abstractions;
+using Watchdog.Emailer.Service.HostedServices;
+using Watchdog.Emailer.Service.Services;
+using Watchdog.Emailer.Service.Settings;
+
+namespace Watchdog.Emailer.Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("Host created.");
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHostedService<EmailService>();
+                    services.AddSingleton<ISendGridClient>(x => 
+                        new SendGridClient(context.Configuration["SendGrid:ApiKey"]));
+                    services.AddSingleton<IEmailSender, SendGridEmailSender>();
+                    services.AddOptions()
+                        .Configure<RabbitMQSettings>(context.Configuration.GetSection("RabbitMQ"))
+                        .Configure<SendGrigSettings>(context.Configuration.GetSection("SendGrid"));
+                });
+        }
+    }
+}
---
+++ backend/Watchdog.Emailer/Watchdog.Emailer.Service/Settings/RabbitMQSettings.cs
@@ -0,0 +1,15 @@
+﻿using Watchdog.RabbitMQ.Shared.Models;
+
+namespace Watchdog.Emailer.Service.Settings
+{
+    public class RabbitMQSettings
+    {
+        public string Uri { get; set; }
+        public QueueSettings Queues { get; set; }
+    }
+
+    public class QueueSettings
+    {
+        public ConsumerSettings EmailerQueueConsumer { get; set; }
+    }
+}
