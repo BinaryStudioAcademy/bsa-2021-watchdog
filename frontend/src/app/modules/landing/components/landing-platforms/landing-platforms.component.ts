fd59be8467e8f778e284310a1f8a571cb7ef1c66
Oleksandr <oleksandr.bovchaliuk@gmail.com>
2021-09-07 14:19:59 +0300

    Update logo

--- frontend/src/app/modules/landing/components/landing-platforms/landing-platforms.component.ts
+++ frontend/src/app/modules/landing/components/landing-platforms/landing-platforms.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 export class LandingPlatformsComponent {
     urls = [
         'https://www.sentry.dev/_assets2/static/f3663bf3904adcea0d835bc0ed00a76e/5e011/dotnet.webp',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/2048px-.NET_Core_Logo.svg.png',
+        'https://d1bd5u3q1t3nu7.cloudfront.net/icons/1411/logo-angular-icon.png',
         'https://www.sentry.dev/_assets2/static/d68867cb09825ed57929417741df8676/5e011/javascript.webp',
         'https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg',
-        'https://d1bd5u3q1t3nu7.cloudfront.net/icons/1411/logo-angular-icon.png',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/2048px-.NET_Core_Logo.svg.png',
         'https://www.sentry.dev/_assets2/static/24ef7069eeae638c5b3759fb927cd8ec/5e011/react.webp',
         'https://www.freeiconspng.com/uploads/c-logo-icon-18.png',
 
