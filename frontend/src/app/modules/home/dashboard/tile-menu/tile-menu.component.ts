f5ae1883951a0c7fc5bc9f0615cb21a8c9ebef2b
KateKubyshka <ekaterina.kubyshka@gmail.com>
2021-09-08 13:38:29 +0300

    Change dropdown tile menu for menu with buttons

--- frontend/src/app/modules/home/dashboard/tile-menu/tile-menu.component.ts
+++ frontend/src/app/modules/home/dashboard/tile-menu/tile-menu.component.ts
@@ -13,11 +13,6 @@ import { ConfirmWindowService } from '@core/services/confirm-window.service';
     styleUrls: ['./tile-menu.component.sass']
 })
 export class TileMenuComponent implements OnInit {
-    menuListItems: MenuItem[] = [];
-    menuValueItems: MenuItem[] = [];
-    menuChartItems: MenuItem[] = [];
-    menuTextItems: MenuItem[] = [];
-    selectedItem?: MenuItem;
 
     @Output()
     closeMenu: EventEmitter<boolean> = new EventEmitter();
@@ -32,14 +27,12 @@ export class TileMenuComponent implements OnInit {
     @Input() orderChanged: boolean;
 
     constructor(
-        private toastNotifications: ToastNotificationService,
         private tileDialogService: TileDialogService,
         private confirmWindowService: ConfirmWindowService
     ) { }
 
     ngOnInit(): void {
-        this.initMenuListItems();
-        this.initMenuChartItems();
+
     }
 
     clearDashboardTiles() {
@@ -60,69 +53,18 @@ export class TileMenuComponent implements OnInit {
         this.saveTilesOrder.emit();
     }
 
-    listItemSelected(item?: MenuItem): void {
-        switch (+item.id) {
-            case TileType.TopActiveIssues:
-                this.selectedItem = item;
-                this.tileDialogService.showTopActiveIssuesCreateDialog(this.userProjects, this.dashboardId, this.tiles);
-                break;
-            default:
-                this.selectedItem = undefined;
-                break;
-        }
+    createActiveIssues() {
+        this.tileDialogService.showTopActiveIssuesCreateDialog(this.userProjects, this.dashboardId, this.tiles);
     }
 
-    chartItemSelected(item?: MenuItem): void {
-        switch (+item.id) {
-            case TileType.IssuesPerTime:
-                this.selectedItem = item;
-                this.tileDialogService.showIssuesPerTimeCreateDialog(this.userProjects, this.dashboardId, this.tiles);
-                break;
-            case TileType.IssuesCount:
-                this.selectedItem = item;
-                this.tileDialogService.showIssuesCountCreateDialog(this.userProjects, this.dashboardId, this.tiles);
-                break;
-            case TileType.HeatMap:
-                this.selectedItem = item;
-                this.tileDialogService.showHeatMapCreateDialog(this.userProjects, this.dashboardId, this.tiles);
-                break;
-            default:
-                this.selectedItem = undefined;
-                break;
-        }
+    createIssuePerTime() {
+        this.tileDialogService.showIssuesPerTimeCreateDialog(this.userProjects, this.dashboardId, this.tiles);
+    }
+    createIssueCount() {
+        this.tileDialogService.showIssuesCountCreateDialog(this.userProjects, this.dashboardId, this.tiles);
     }
 
-    private initMenuListItems(): void {
-        this.menuListItems = [
-            {
-                id: TileType.TopActiveIssues.toString(),
-                label: 'Top Active Issues',
-                icon: 'pi pi-ban',
-                command: event => this.listItemSelected(event.item)
-            },
-        ];
-    }
-
-    private initMenuChartItems(): void {
-        this.menuChartItems = [
-            {
-                id: TileType.IssuesPerTime.toString(),
-                label: 'Issues Per Time',
-                icon: 'pi pi-chart-bar',
-                command: event => this.chartItemSelected(event.item)
-            },
-            {
-                id: TileType.IssuesCount.toString(),
-                label: 'Issues Count',
-                icon: 'pi pi-th-large',
-                command: event => this.chartItemSelected(event.item)
-            },
-            {
-                id: TileType.HeatMap.toString(),
-                label: 'Heat map',
-                icon: 'pi pi-heart',
-                command: event => this.chartItemSelected(event.item)
-            },
-        ];
+    createHeatMap() {
+        this.tileDialogService.showHeatMapCreateDialog(this.userProjects, this.dashboardId, this.tiles);
     }
 }
